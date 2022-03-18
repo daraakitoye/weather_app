ea14f83365e0c45397288c7d617fba79f8847860
Dara A <daraakitoye1@gmail.com>
2022-03-18 01:26:53 -0600

    Adds event listeners

--- app.js
+++ app.js
@@ -11,6 +11,30 @@ window.addEventListener("load", () => {
 
       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
       console.log(base);
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather;
+          const { sunrise, sunset } = data.sys;
+
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const fahrenheit = (temp * 9) / 5 + 32;
+          //converting epoch time to gmt
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+        });
     });
   }
 });
+
+const iconImg = document.getElementById("weather-icon");
+const loc = document.querySelector("#location");
+const tempC = document.querySelector(".c");
+const tempF = document.querySelector(".f");
+const desc = document.querySelector(".desc");
+const sunriseDOM = document.querySelector(".sunrise");
+const sunsetDOM = document.querySelector(".sunset");
